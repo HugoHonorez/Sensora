eaf791a49f555fa99fc3bb04c626700085b8867c
HugoHonorez <230518@umons.ac.be>
2025-06-22 23:34:43 +0200

    Delete Site directory

--- Site/nav.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    const sidenav = document.getElementById('sidenav');
-    const content = document.getElementById('content');
-    
-    const isOpen = sidenav.style.left === '0px';
-
-    sidenav.style.left = isOpen ? '-250px' : '0px';
-    content.classList.toggle('open', !isOpen);
-}
